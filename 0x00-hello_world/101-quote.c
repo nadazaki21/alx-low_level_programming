fccf71b86a213dd42de0a4a16522ce57395c5173
nadazkai21 <nadazaki019@gmail.com>
2023-10-05 21:08:58 +0300

    return 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-* main - maun fun
+* main - main fun
 *
 * puts function practise
-* Return: return 0 for no errors
+* Return: here the error message displayed is going to be the same 
+* as the output beacause the  streams , the error and the output
+* were mereged toghether, if  you run it, it will produce the error
+*and the output 
 */
 int main(void)
 {
 dup2(2, 1); /* function to duplicat ethe file descriptor*/
-puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 {return 1 ; }
 }
