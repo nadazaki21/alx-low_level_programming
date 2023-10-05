3764382afbd3996b0517026facbe775a76754590
nadazkai21 <nadazaki019@gmail.com>
2023-10-05 19:24:19 +0300

    5,6,4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-
-int main (){
-
-    puts("\"Programming is like building a multilingual puzzle"); 
-
-    return 0 ; 
+/*
+* puts function practise
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+{return 0 ; }
 }
