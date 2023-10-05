94bf09819e500c83bbad8f06880586db1b138978
nadazkai21 <nadazaki019@gmail.com>
2023-10-05 21:06:47 +0300

    return 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - maun fun
+*
+* puts function practise
+* Return: return 0 for no errors
+*/
+int main(void)
+{
+dup2(2, 1); /* function to duplicat ethe file descriptor*/
+puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+{return 1 ; }
+}
