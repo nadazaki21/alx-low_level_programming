a9bdf46e5b2676231ff70ea8620f17546c830728
nadazkai21 <nadazaki019@gmail.com>
2023-10-08 15:03:40 +0300

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,21 +7,21 @@
  */
 int main(void)
 {
-   int num1;
-   int num2;
+int num1;
+num1 = 0;
 
-   num1 = 0 ; 
-   num2 = 9;
 
-   while (num1 <= 9 && num2 >=0)
-   {
-	 putchar(num1 + '0');
-	 putchar(',');
-	 putchar(' ');
-	 num1++;
-	 num2--;
-   }
-   putchar('\n');
+	while (num1 <= 9)
+	{
+		putchar(num1 + '0');
+		if (num1 != 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		num1++;
+	}
+	putchar('\n');
 
 
 return (0);
