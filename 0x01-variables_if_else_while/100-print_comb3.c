e566021ba6ebda2c29560d903ed72c683aa74284
nadazkai21 <nadazaki019@gmail.com>
2023-10-08 23:34:08 +0300

    10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,9 +7,11 @@
  */
 int main(void)
 {
-	for (int i = 0 ; i < 9; i++)
+	int i; 
+	int j;
+	for (i = 0 ; i < 9; i++)
 	{
-		for (int j = i + 1 ; j <= 9 ; j++)
+		for (j = i + 1 ; j <= 9 ; j++)
 		{
 			putchar(i + '0');
 			putchar (j + '0');
