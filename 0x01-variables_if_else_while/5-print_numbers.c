74d5e1666500650300e531689ea9dcb35e49a384
nadazkai21 <nadazaki019@gmail.com>
2023-10-08 13:56:20 +0300

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main function
+ * Return:The program returns 0 as  sucessful excution
+ */
+int main(void)
+{
+char num;
+num = 0x30;
+
+
+	while (num <= 0x39)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+return (0);
+}
