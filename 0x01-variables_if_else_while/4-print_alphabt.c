0c6d37053cc220a48dc6ac18505bf33f037eec4c
nadazkai21 <nadazaki019@gmail.com>
2023-10-08 13:50:48 +0300

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main function
+ * Return:The program returns 0 as  sucessful excution
+ */
+int main(void)
+{
+char letter;
+letter = 0x61;
+
+
+	while (letter <= 0x7A)
+	{
+        if (letter != 0x71 && letter != 0x65)
+        {
+            putchar(letter);
+        }
+		
+		letter++;
+	}
+	putchar('\n');
+return (0);
+}
