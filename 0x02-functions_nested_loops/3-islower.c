fcb5e1ab7515b117aeaed13ad3a45984303a37d4
nadazkai21 <nadazaki019@gmail.com>
2023-10-10 21:34:33 +0300

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,18 +2,24 @@
 #include "main.h"
 #include <ctype.h>
 /**
-* print_alphabet_x10 - function for printing the alphabets
+* _islower - checking if the ltter is lower case or not
+* @c: the char to be checked
+* Return: if it is lower case it will return 1, other than
+* that is it is a ccapital
+* letter or is a number it will return 0
 */
 
 int _islower(int c)
 {
-    if (islower(c) != 0)
-    {
-        return 1; 
-    }
-    else
-    {
-        return 0; 
-    }
+	if (islower(c) != 0)
+	{
+		return (1);
+	}
+	else /*if both were if an error would be produces beacuase there would be \
+	 * no return to cover all of the cases if the if fail
+	 */
+	{
+		return (0);
+	}
 }
 
