c7fcdbb53186d5c282ec8a730387788378c8969a
nadazkai21 <nadazaki019@gmail.com>
2023-10-10 22:29:14 +0300

    6

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+* print_last_digit- checking if the ltter is lower case or not
+* @n: the char to be checked
+* Return: if it is lower case it will return 1, other than
+* that is it is a ccapital
+* letter or is a number it will return 0
+*/
+
+int print_last_digit(int n)
+{
+	int last_digit;
+    last_digit = n % 10;
+	
+
+return (last_digit);
+}
+
