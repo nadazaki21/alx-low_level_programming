1f25368ea0ee4b344af9fee856f2fcc6cc725c9a
nadazkai21 <nadazaki019@gmail.com>
2023-10-10 21:01:26 +0300

    3

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,11 +1,20 @@
 #include "main.h"
+
 /**
- * main - check the code
+ * main - check the code.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    _islower('a');
+    int r;
+
+    r = _islower('H');
+    _putchar(r + '0');
+    r = _islower('o');
+    _putchar(r + '0');
+    r = _islower(108);
+    _putchar(r + '0');
+    _putchar('\n');
     return (0);
 }
