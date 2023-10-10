0ff71a8bf1e8a87c37bea4c2698ffe423210bdd1
nadazkai21 <nadazaki019@gmail.com>
2023-10-11 01:27:54 +0300

    101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n = 1024;
+	int i;
+	int sum = 0;
+
+	for (i = 1 ; i < 1024 ; i++)
+	{
+		if ((1024 % i) == 0)
+		{
+			sum = sum + i;
+		}
+	}
+}
