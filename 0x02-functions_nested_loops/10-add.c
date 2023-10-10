b2b829b0c2dfb4c217368d84a6e9d991ae34f347
nadazkai21 <nadazaki019@gmail.com>
2023-10-11 00:35:50 +0300

    10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 /**
 * add - checking if the ltter is lower case or not
+* @i : num1
+* @j : num2
 * Return: if it is lower case it will return 1, other than
 * that is it is a ccapital
 * letter or is a number it will return 0
@@ -10,7 +12,7 @@
 int add(int i, int j)
 {
 
-	return (i+j);
+	return (i + j);
 
 }
 
