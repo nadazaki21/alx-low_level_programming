b74e152814bcba618fa07928a5a94ba7c3dae84f
nadazkai21 <nadazaki019@gmail.com>
2023-10-10 23:49:16 +0300

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,32 +3,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* jack_bauer - checking if the ltter is lower case or not
+* times_table - checking if the ltter is lower case or not
 * Return: if it is lower case it will return 1, other than
 * that is it is a ccapital
 * letter or is a number it will return 0
 */
 
-void jack_bauer(void)
+void times_table(void)
 {
 	int i;
 	int j;
-    int result; 
+	int result;
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		for (j = 0; j <= 9 ; j++)
 		{
-            result = i * j ; 
+			result = i * j;
 			printf("%d", result);
-            putchar(',');
-            putchar(' ');
-            putchar(' ');
+			putchar(',');
+			putchar(' ');
+			putchar(' ');
 
 		}
 
-            putchar('\n');
-        
+			putchar('\n');
+
 	}
 
 }
