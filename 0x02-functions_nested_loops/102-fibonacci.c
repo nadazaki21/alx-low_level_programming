b4d3182137747f52f9e942e04c6f69105e976fdc
nadazkai21 <nadazaki019@gmail.com>
2023-10-11 02:00:36 +0300

    102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,18 +11,31 @@ int main(void)
     long  int result = 3;
     long  int before_last = 2;
 
-    last = result ; 
-    for (i = 0 ; i < n ; i++)
+    last = result ;
+
+    printf("%d", 1);
+    putchar(',');
+    putchar(' ');
+    printf("%d", 2); 
+    putchar(',');
+    putchar(' ');
+    
+    for (i = 3 ; i < n ; i++)
     {
+        
         result = last + before_last;
         before_last = last;
         last = result;
 
         printf("%ld",result);
-        putchar(',');
-        putchar(' ');
 
-
+        if ( i !=  (n-1))
+        {
+            putchar(',');
+            putchar(' ');
+        }
+        
+        putchar('\n');
     }
 
 	return (0);
