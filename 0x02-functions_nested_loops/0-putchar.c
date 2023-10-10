5729d9af3139f27478b3ae5ae52d2e8562663b8b
nadazkai21 <nadazaki019@gmail.com>
2023-10-10 19:24:48 +0300

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include "main.h"
+/**
+ * main - main function of the program
+ * Return: return 0 in case of successful compilation
+*/
 
 int main(void)
 {
-    printf("_putchar\n");
+printf("_putchar\n");
+
+return (0);
 
 }
