85859f23e882e28a5ce0425dbb5e667c66097fe0
nadazkai21 <nadazaki019@gmail.com>
2023-10-10 22:16:01 +0300

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,21 +8,21 @@
 * letter or is a number it will return 0
 */
 
-int print_sign (int n)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
-        _putchar('+');
+		_putchar('+');
 		return (1);
 	}
-    else if (n < 0)
-    {
-        _putchar('-');
-		return (-1);
-    }
-	else 
+	else if (n < 0)
 	{
-        _putchar('0');
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
 		return (0);
 	}
 }
