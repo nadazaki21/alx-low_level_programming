41dee67bbcf1ffa12e4ac52ccaef8dcfa0d3f4f8
nadazkai21 <nadazaki019@gmail.com>
2023-10-10 21:20:56 +0300

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,15 +7,13 @@
 
 int _islower(int c)
 {
-    if (islower(c) == 0)
-    {
-        return (0);
-    }
-
     if (islower(c) != 0)
     {
-        return (1);
+        return 1; 
     }
-    
-
+    else
+    {
+        return 0; 
+    }
 }
+
