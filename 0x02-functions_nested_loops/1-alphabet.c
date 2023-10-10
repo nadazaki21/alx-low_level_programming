9539f202fabbc4bb511b84ebe313fe4197338518
nadazkai21 <nadazaki019@gmail.com>
2023-10-10 19:39:35 +0300

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - main function of the program
+ * Return: return 0 in case of successful compilation
+*/
+void print_alphabet(void);
+int main(void)
+{
+printf("_putchar\n");
+
+putchar('a');
+
+return (0);
+
+}
+
+void print_alphabet(void)
+{
+_putchar('a');
+}
