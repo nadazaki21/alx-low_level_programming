6ee2ea3c91b296061285656869cc6115922ebfe5
nadazkai21 <nadazaki019@gmail.com>
2023-10-10 19:45:08 +0300

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,9 @@
  * main - main function of the program
  * Return: return 0 in case of successful compilation
 */
-void print_alphabet(void);
 int main(void)
 {
 printf("_putchar\n");
-
-putchar('a');
 
 return (0);
 
