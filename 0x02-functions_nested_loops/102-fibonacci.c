0b1ced0955586d1afc0cca3032565e35e25b6602
nadazkai21 <nadazaki019@gmail.com>
2023-10-11 02:06:00 +0300

    102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,36 +7,33 @@
 int main(void)
 {
 	int n = 50;
-    long  int i, last;
-    long  int result = 2;
-    long  int before_last = 1;
+	long  int i, last;
+	long  int result = 2;
+	long  int before_last = 1;
 
-    last = result ;
+	last = result;
 
-    printf("%d", 1);
-    putchar(',');
-    putchar(' ');
-    printf("%d", 2); 
-    putchar(',');
-    putchar(' ');
+	printf("%d", 1);
+	putchar(',');
+	putchar(' ');
+	printf("%d", 2);
+	putchar(',');
+	putchar(' ');
 
-    for (i = 2 ; i < n ; i++)
-    {
-        
-        result = last + before_last;
-        before_last = last;
-        last = result;
+	for (i = 2 ; i < n ; i++)
+	{
+		result = last + before_last;
+		before_last = last;
+		last = result;
 
-        printf("%ld",result);
+		printf("%ld", result);
 
-        if ( i !=  (n-1))
-        {
-            putchar(',');
-            putchar(' ');
-        }
-        
-        
-    }
-    putchar('\n');
+		if (i !=  (n - 1))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 	return (0);
 }
