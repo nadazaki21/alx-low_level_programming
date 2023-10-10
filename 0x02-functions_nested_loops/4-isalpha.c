afd93e30a0ffbb18bff50bb23b40dac0b4b6393d
nadazkai21 <nadazaki019@gmail.com>
2023-10-10 21:37:58 +0300

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+* _isalpha - checking if the ltter is lower case or not
+* @c: the char to be checked
+* Return: if it is lower case it will return 1, other than
+* that is it is a ccapital
+* letter or is a number it will return 0
+*/
+
+int _isalpha(int c)
+{
+	if (isalpha(c) != 0)
+	{
+		return (1);
+	}
+	else /*if both were if an error would be produces beacuase there would be \
+	 * no return to cover all of the cases if the if fail
+	 */
+	{
+		return (0);
+	}
+}
+
--- 0x02-functions_nested_loops/test.c
+++ 0x02-functions_nested_loops/test.c
@@ -6,20 +6,20 @@ int main()
     char c;
 
     c='T';
-    printf(" %d\n", islower(c));
+    printf(" %d\n", isalpha(c));
 
     c='D';
-    printf("%d\n", islower(c));
+    printf("%d\n", isalpha(c));
 
     c= 9; 
-  printf("%d\n", islower(c));
+  printf("%d\n", isalpha(c));
 
   c= 'a'; 
-  printf("%d\n", islower(c));
+  printf("%d\n", isalpha(c));
 
 
-c= '108'; 
-  printf("%d\n", islower(c));
+c ='108'; 
+  printf("%d\n", isalpha(c));
 
     return 0;
 }
