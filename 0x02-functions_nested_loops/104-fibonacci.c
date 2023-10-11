643e07b7d38355064185f21dc45a588cd30ca05d
nadazkai21 <nadazaki019@gmail.com>
2023-10-11 13:14:26 +0300

    0

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n = 98;
+	long  int i, last;
+	long  int result = 2;
+	long  int before_last = 1;
+
+	last = result;
+
+	printf("%d", 1);
+	putchar(',');
+	putchar(' ');
+	printf("%d", 2);
+	putchar(',');
+	putchar(' ');
+
+	for (i = 2 ; i < n ; i++)
+	{
+		result = last + before_last;
+		before_last = last;
+		last = result;
+
+		printf("%ld", result);
+
+		if (i !=  (n - 1))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
