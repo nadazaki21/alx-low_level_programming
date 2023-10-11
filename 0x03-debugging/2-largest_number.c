d2f7a96c26c3350beaf24a3058031e2d664ba2d3
nadazkai21 <nadazaki019@gmail.com>
2023-10-11 22:09:25 +0300

    2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -21,10 +21,32 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else
+	else if (a == b)
 	{
-		largest = c;
+		if (a > c)
+			largest = a;
+		else
+			largest = c;
 	}
-
+	else if (a == c)
+	{
+		if (a > b)
+			largest = b;
+		else
+			largest = b;
+	}
+	else if (b == c)
+	{
+		if (a > b)
+			largest = a;
+		else
+			largest = b;
+	}
+	/**
+	 * else
+	*{
+	*	largest = c;
+	*}
+	*/
 	return (largest);
 }
