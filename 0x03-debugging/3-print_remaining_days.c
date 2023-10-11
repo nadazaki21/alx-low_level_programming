c4c6c622114da56d7033d2c8d0cd5e8d630e208e
nadazkai21 <nadazaki019@gmail.com>
2023-10-11 15:38:21 +0300

    3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -8,10 +8,10 @@
 */
 int is_leap_year(int year)
 {
-    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-        return 1;
-    else
-        return 0;
+	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+		return (1);
+	else
+		return (0);
 }
 
 /**
@@ -24,29 +24,32 @@ int is_leap_year(int year)
 */
 void print_remaining_days(int month, int day, int year)
 {
-    int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    int isLeap = is_leap_year(year);
+	int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	int isLeap = is_leap_year(year);
+	int remaining_days;
+	int i;
 
-    if (month < 1 || month > 12 || day < 1 || day > days_in_month[month])
-    {
-        printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-        return;
-    }
+	if (month < 1 || month > 12 || day < 1 || day > days_in_month[month])
+	{
+		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
 
-    int remaining_days = 0;
+	}
 
-    for (int i = month + 1; i <= 12; i++)
-    {
-        remaining_days += days_in_month[i];
-        if (isLeap && i == 2)
-            remaining_days++; // Leap year: February has 29 days
-    }
+	remaining_days = 0;
 
-    remaining_days += days_in_month[month] - day;
+	for (i = month + 1; i <= 12; i++)
+	{
+		remaining_days += days_in_month[i];
+		if (isLeap && i == 2)
+			remaining_days++;
 
-    if (isLeap && month <= 2)
-        remaining_days++; // Leap year: February has 29 days
+	remaining_days += days_in_month[month] - day;
 
-    printf("Day of the year: %d\n", day + remaining_days);
-    printf("Remaining days: %d\n", isLeap ? 366 - (day + remaining_days) : 365 - (day + remaining_days));
+	if (isLeap && month <= 2)
+		remaining_days++;
+
+	printf("Day of the year: %d\n", day + remaining_days);
+	printf("Remaining days: %d\n",
+	 isLeap ? 366 - (day + remaining_days) : 365 - (day + remaining_days));
+	}
 }
