815a11be145908ff914ca5a49f93d50b9dbf3e3a
nadazkai21 <nadazaki019@gmail.com>
2023-10-12 00:04:41 +0300

    3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,7 +13,8 @@
 void print_remaining_days(int month, int day, int year)
 {
 	int current_day;
-	current_day = (month,day);
+	current_day = convert_day(month, day);
+
     if ((year % 4 == 0) || ((year % 400 == 0) && (year % 100 == 0)))
     {
         if (month >= 2)
