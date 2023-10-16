0909040aab48ee50dd60bb73e2c9b6f9e10d3ba8
nadazkai21 <nadazaki019@gmail.com>
2023-10-17 00:27:53 +0300

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdio.h>
 /**
  * puts2 - prints every other char in string
  * @str: pointer to sting to be printed
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,2 +1,3 @@
 void print_rev(char *s);
 void rev_string(char *s);
+void puts_half(char *str);
