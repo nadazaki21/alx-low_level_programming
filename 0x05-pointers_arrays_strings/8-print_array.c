569c0786698acb604ffb489534fca2ab9ef3cb9c
nadazkai21 <nadazaki019@gmail.com>
2023-10-17 01:05:31 +0300

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * print_array - prints array of integegrs
+ * @a: pointer to array
+ * @n: size of array
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n ; i++)
+	{
+		printf("%d, ", *(a + i));
+	}
+
+}
