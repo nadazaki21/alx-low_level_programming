629da4985a67874cdb77de186623e840034afe9a
nadazkai21 <nadazaki019@gmail.com>
2023-10-17 02:03:29 +0300

    last

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,7 +9,7 @@ void puts_half(char *str)
 	int len = strlen(str);
 	int i;
 
-	for (i = 0 ; i < ((len - 1) / 2) ; i++)
+	for (i = ((len - 1) / 2) ; i < len ; i++)
 	{
 		
 		printf("%c", *(str + i));
