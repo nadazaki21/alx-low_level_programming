cc623cee13e89f9e53660bbd06abb80d272e522b
nadazkai21 <nadazaki019@gmail.com>
2023-10-17 01:51:40 +0300

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include <string.h>
+/**
+ * _strcpy - copy the string to the buffer
+ * @dest: destination
+ * @src: source
+ * Return: the pointer to the buffer
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int len = strlen(src);
+	int i;
+
+	for (i = 0 ; i <= len ; i++)
+	{
+		*(dest + 1) = *(src + i);
+	}
+
+}
