671b22eb7678a0272c3bb98b9f018aad392ac286
nadazkai21 <nadazaki019@gmail.com>
2023-10-17 01:59:23 +0300

    last

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,14 +9,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int len = strlen(src);
 	int i;
-	static int address;
 
 	for (i = 0 ; i <= len ; i++)
 	{
 		*(dest + i) = *(src + i);
 	}
 
-	address = &dest;
-	return (address);
+	return (dest);
 
 }
