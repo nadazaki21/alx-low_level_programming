52576cf73fc38b0ecdb36f8ab5f38ca3b7b15053
nadazkai21 <nadazaki019@gmail.com>
2023-10-18 20:58:20 +0300

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,23 @@
+/**
+ * _strncpy - copies string to n vharecter to destination
+ * @dest: destination
+ * @src: src
+ * @n: number of charecters to be copied
+ * Return: tthe pointer to the copies string
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i;
-    for (i = 0; i <n; i++)
-    {
-        dest[i] = src[i];
-    }
-    
+	int i;
 
-    return (&dest);
-    
+	static char *ptr;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	ptr = dest;
+
+	return (ptr);
+
 }
