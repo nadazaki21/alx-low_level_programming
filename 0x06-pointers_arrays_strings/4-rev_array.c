ed1801c6bf46310e4fa4e5091d491e7ce4ae05da
nadazkai21 <nadazaki019@gmail.com>
2023-10-18 23:47:32 +0300

    4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,17 +7,17 @@ void reverse_array(int *a, int n)
 {
 	int i;
 	int j = 0;
-	int *temp;
+	int temp[1000];
 
-	for (i = (n - 1); i >= 0; i++)
+	for (i = (n - 1); i >= 0; i--)
 	{
-		*(temp + j) = *(a + i);
+		temp[j] = *(a + i);
 		j++;
 	}
 
 	for (i = 0; i < n; i++)
 	{
-		*(a + i) = *(temp + i);
+		*(a + i) = temp[i];
 	}
 
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,2 +1,3 @@
 int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
+void reverse_array(int *a, int n);
