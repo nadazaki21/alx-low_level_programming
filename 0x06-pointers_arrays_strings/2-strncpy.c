84ddecd3c1081c2844bf61ddc813f8ebe5c26e48
nadazkai21 <nadazaki019@gmail.com>
2023-10-18 21:23:02 +0300

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -26,12 +26,10 @@ char *_strncpy(char *dest, char *src, int n)
 	}
 	else  /* src is less than the allowed len*/
 	{
-		
-		for (i = 0; i < (countersrc + 1); i++)
+		for (i = 0; i < (countersrc + 2); i++)
 		{
 			dest[i] = src[i];
 		}
-		
 	}
 
 	ptr = dest;
