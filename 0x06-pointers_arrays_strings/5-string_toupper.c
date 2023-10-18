aa8f61f66fa30f30620b55f70319eb4fb129109d
nadazkai21 <nadazaki019@gmail.com>
2023-10-19 00:06:54 +0300

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+/**
+ * string_toupper - changes awhole string into an upper case
+ * @n: pointer to string
+ * Return: pointer to string after changing
+*/
+char *string_toupper(char * n)
+{
+    int i = 0;
+    while (*(n+i) != '\0')
+    {
+        *(n+i) = toupper(*(n+i));
+        i++;
+    }
+    
+    return (n);
+}
