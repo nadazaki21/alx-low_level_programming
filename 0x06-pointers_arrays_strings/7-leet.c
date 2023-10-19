bda8c1702f4460d443d6c446f902de2d6a0ce00b
nadazkai21 <nadazaki019@gmail.com>
2023-10-19 20:04:11 +0300

    7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,12 +5,12 @@
 */
 char *leet(char *n)
 {
-	int i=0;
+	int i = 0;
 	int j;
 
 	char cap_letters[5] = {'A', 'E', 'O', 'T', 'L'};
 	char small_letters[5] = {'a', 'e', 'o', 't', 'l'};
-	char numbers[5] = {'4', '3', '0', '7' ,'1'};
+	char numbers[5] = {'4', '3', '0', '7', '1'};
 
 	while (*(n + i) != '\0')
 	{
