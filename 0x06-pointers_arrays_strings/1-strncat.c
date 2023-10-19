944e88a007e55b3adc93da46932915c2b22b6141
nadazkai21 <nadazaki019@gmail.com>
2023-10-19 22:14:30 +0300

    0,1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,49 @@
+/**
+ * _strncat - function to concatinate 2 stringgs to n bytes max
+ * @dest: dest string
+ * @src: source string
+ * @n: max number of bytes
+ * Return: pointer to dest string after edit
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	int lendest = 0, lensrc = 0;
+
+	/* get the length of both strings without the null char*/
+	while (*(dest + lendest) != '\0')
+	{
+		lendest++;
+	}
+
+	while (*(src + lensrc) != '\0')
+	{
+		lensrc++;
+	}
+
+	if (n < lensrc)
+	{
+		j = 0;
+
+		for (i = 0 ; i <= lensrc; i++)
+		{
+			*(dest + lendest + j) = src[i];
+			j++;
+		}
+
+	}
+	else
+	{
+		j = 0;
+
+		for (i = 0 ; i < n; i++)
+		{
+			*(dest + lendest + j) = src[i];
+			j++;
+		}
+
+	}
+
+
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ char *string_toupper(char * n);
 char *cap_string(char *);
 char *leet(char *);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
