b2f514df3524325d6e691a5bd85bb3fde9cf1bde
nadazkai21 <nadazaki019@gmail.com>
2023-10-19 19:22:24 +0300

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,52 +1,46 @@
+/**
+ * _strcmp - strings comaprison
+ * @s1: string 1
+ * @s2: strig 2
+ * Return: the difference between the difference char
+*/
 int _strcmp(char *s1, char *s2)
 {
-    int i;
-    int lenS1 = 0;
-    int lenS2 = 0;
-    int len; /*len with which i will compare*/
+	int i, len;
+	int lenS1 = 0, lenS2 = 0;
 
-    while (*(s1+lenS1) != '\0')
-    {
-            lenS1++;
-    }
-
-    while (*(s2+lenS2) != '\0')
-    {
-            lenS2++;
-    }
-
-    /* finding out which string is longer, we shall take the shorter */
-    if (lenS1 > lenS2)
-    {
-        len = lenS2;
-    }
-    else if (lenS1 < lenS2)
-    {
-        len = lenS1;
-    }
-    else
-    {
-        len = lenS1;
-    }
-
-    for (i = 0; i < len; i++)
-    {
-        if (s1[i] > s2[i])
-        {
-            return (-15);
-            break;
-        }
-        else if (s1[i] < s2[i])
-        {
-            return (15);
-            break;
-        }
-         
-    }
-
-    if ( lenS1 == lenS2)
-    {
-        return (0);
-    }
-    else
+	while (*(s1 + lenS1) != '\0')
+	{
+		lenS1++;
+	}
+	while (*(s2 + lenS2) != '\0')
+	{
+		lenS2++;
+	}
+	/* find the shorter of the 2 strings*/
+	if (lenS1 > lenS2)
+	{
+		len = lenS2;
+	}
+	else if (lenS1 < lenS2)
+	{
+		len = lenS1;
+	}
+	else
+	{
+		len = lenS1; /* two lenghts are equal*/
+	}
+	for (i = 0; i < len; i++)
+	{
+		if (s1[i] > s2[i] || s1[i] < s2[i])
+		{
+			return ((s1[i] - s2[i]));
+		}
+	}
+	if (lenS1 == lenS2)
+		return (0);
+	else if (lenS1 > lenS2)
+		return ((int)(*(s1 + len + 1)));
+	else
+		return ((int)((*(s2 + len + 1))) * -1);
 }
