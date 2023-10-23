d985c9b467fcdab9bcbf67ad148f73dba389d70d
nadazkai21 <nadazaki019@gmail.com>
2023-10-24 01:09:03 +0300

    100

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,4 +1,11 @@
+/**
+ * set_string - making a ponter point to another string
+ * thorugh a pointer to a pointer
+ * @s: pointer to pointer, the one we want to change
+ * @to: pointer to string we want to switch to
+*/
 void set_string(char **s, char *to)
 {
-    *s = to ; 
+	*s = to ; 
 }
+
