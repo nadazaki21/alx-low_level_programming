77adfea97b21d883c692e7274d1fa81afd234c8d
nadazkai21 <nadazaki019@gmail.com>
2023-10-25 22:53:47 +0300

    5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,43 @@
+/**
+ * sqrt_helper - checks if the number is
+ * @n: number for which I wil find the sqaure root
+ * @guess: the number that might be the sqaure root
+ * Return: the result, either -1 if no sqaure
+ * or the result of the sqaure
+*/
+int sqrt_helper(int n, int guess)
+{
+	if (guess == 0)
+	{
+		return (-1);
+	}
+
+	else if ((guess * guess) != n)
+	{
+		guess--;
+		return (sqrt_helper(n, guess));
+	}
+
+	else
+	{
+		return (guess);
+	}
+}
+
+/**
+ * _sqrt_recursion - checks if the number is
+ * @n: number for which I wil find the sqaure root
+ * Return: the result, either -1 if no sqaure
+ * or the result of the sqaure
+*/
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (sqrt_helper(n, n));
+	}
+}
