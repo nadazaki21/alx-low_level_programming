644c183661e1b845c94848edbf9f58a7f7dd5e15
nadazkai21 <nadazaki019@gmail.com>
2023-10-28 00:24:05 +0200

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - program that prints its name, followed by a new line.
+ * @argc: number of arguments in command of running
+ * the proram
+ * @argv: the actull paarmeters if the running command
+*/
+int main(int argc, char *argv)
+{
+	printf("%s\n", argv[0]);
+}
