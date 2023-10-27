00dcd9a4d6b27260d0e22319ef6a5128b8fe4f89
nadazkai21 <nadazaki019@gmail.com>
2023-10-28 01:32:52 +0200

    $

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,20 +20,20 @@ int main(int argc, char **argv)
 	for (i = 0; i < argc; i++)
 	{
 		for (j = 0; argv[i][j]; j++)
-        {
-            if ((argv[i][j]) < 48 || (argv[i][j] > 57))
-            {
-                printf("Error\n");
-                return 1;
-            }
-        }
-
-        sum += atoi(argv[i]);
+		{
+			if ((argv[i][j]) < 48 || (argv[i][j] > 57))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum += atoi(argv[i]);
+			}
+		}
 	}
 
 	printf("%d\n", sum);
 
 	return (0);
 }
-
-
