f74fbb14606c0fb44275a8e3e7844c162621b9d4
nadazkai21 <nadazaki019@gmail.com>
2023-10-28 00:26:34 +0200

    0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - program that prints the number of arguments passed into it.
+ * @argc: number of arguments in command of running
+ * the proram
+ * @argv: the actull paarmeters if the running command
+*/
+int main(int argc, char *argv)
+{
+	printf("%d\n", argc);
+}
