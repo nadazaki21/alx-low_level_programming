b56695335d90c1c97eefb900aadcf92fdcce97d8
nadazkai21 <nadazaki019@gmail.com>
2023-10-28 00:28:30 +0200

    0

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,8 +4,11 @@
  * @argc: number of arguments in command of running
  * the proram
  * @argv: the actull paarmeters if the running command
+ * Return: 0 on success
 */
 int main(int argc, char *argv)
 {
 	printf("%d\n", argc);
+
+	return (0);
 }
