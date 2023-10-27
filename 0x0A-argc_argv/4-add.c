240ab79f2c9e829c4d53a7aa70d6e7038d8faf86
nadazkai21 <nadazaki019@gmail.com>
2023-10-28 01:16:42 +0200

    3

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers.
+ * @argc: number of arguments in command of running
+ * the proram
+ * @argv: the actull paarmeters if the running command
+ * Return: 0 on success
+*/
+int main(int argc, char **argv)
+{
+	int i;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+	}
+
+	for (i = 0; i < argc; i++)
+	{
+		if ((argv[i] > 48) && (argv[i] < 57))
+		{
+			sum = sum + atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
+
+
