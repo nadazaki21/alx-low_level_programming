7ca438c85fd2fdef9113bc705a3f7e477a402304
nadazkai21 <nadazaki019@gmail.com>
2023-10-30 10:01:36 +0200

    0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+/**
+ * create_array - create_array
+ * @size: size of array
+ * @c:the charceter to be assigned to the array
+*/
+char *create_array(unsigned int size, char c)
+{
+    char *ptr;
+    ptr = malloc(sizeof(char) * size);
+
+    if (ptr == NULL || size == 0)
+    {
+        return NULL;
+    }
+    else
+    {
+        unsigned int i;
+
+        for (i = 0; i < size; i++)
+        {
+            ptr[i] = c;
+        }
+        return ptr;
+    }
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,6 @@
+#ifndef MY_HEADER
+#define MY_HEADER
+
+char *create_array(unsigned int size, char c);
+
+#endif 
