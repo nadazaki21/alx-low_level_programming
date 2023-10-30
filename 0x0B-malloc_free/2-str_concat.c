087febefe274ccc915c5ee50361eff7bf7285aec
nadazkai21 <nadazaki019@gmail.com>
2023-10-30 16:10:02 +0200

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,72 +10,63 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
-	int lens1 = 0, lens2 = 0;
-	int i, position;
+	int lens1 = 0, lens2 = 0, i, position;
 	
-
 	if (s1 == NULL && s2 == NULL)
 	{
 		ptr = malloc(sizeof(char) * 1);
-		ptr[0] = '\0';
+		ptr[0] = '\0'; /*EMPTY STRING*/
 		return ptr;
-	}
-	 	
-		
+	}	
 	else if (s1 == NULL)
 	{
 		while (*(s2 + lens2) != '\0')
-		{
 			lens2++;
-		}
+
 		ptr = malloc(sizeof(char) * (lens2) + 1);
+
 		if (ptr == NULL)
 			return (NULL);
+
 		for (i = 0; i <= lens2 ; i++)
 			ptr[i] = s2[i];
-
-		
 	}
 	else if (s2 == NULL)
 	{
 		while (*(s1 + lens1) != '\0')
-		{
 			lens1++;
-		}
+
 		ptr = malloc(sizeof(char) * (lens1) + 1);
+
 		if (ptr == NULL)
 			return (NULL);
 
 		for (i = 0; i <= lens1 ; i++)
 			ptr[i] = s1[i];
-		
-		
 	}
 	else
 	{
 		while (*(s1 + lens1) != '\0')
-		{
 			lens1++;
-		}
+		
 		while (*(s2 + lens2) != '\0')
-		{
 			lens2++;
-		}
-		/* allocate memory*/
+	
 		ptr = malloc(sizeof(char) * (lens1 + lens2) + 1);
+
 		if (ptr == NULL)
 			return (NULL);
-		/* concatinate the 2 strinsg togther*/
+
 		for (i = 0; i < lens1; i++)
-		{
 			ptr[i] = s1[i];
-		}
+		
 		position = lens1;
+
 		for (i = 0 ; i <= lens2; i++)
 		{
 			ptr[position] = s2[i];
 			position++;
-		}
+		}		
 	}
 	return ptr;
 }
