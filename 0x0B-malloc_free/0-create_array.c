a1028c7f30a6de9e4d248f53d90888d46dcf0b59
nadazkai21 <nadazaki019@gmail.com>
2023-10-30 10:03:52 +0200

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,24 +3,26 @@
  * create_array - create_array
  * @size: size of array
  * @c:the charceter to be assigned to the array
+ * Return: NULL on error or size 0, the pointe ron sucess
 */
 char *create_array(unsigned int size, char c)
 {
-    char *ptr;
-    ptr = malloc(sizeof(char) * size);
+	char *ptr;
 
-    if (ptr == NULL || size == 0)
-    {
-        return NULL;
-    }
-    else
-    {
-        unsigned int i;
+	ptr = malloc(sizeof(char) * size);
 
-        for (i = 0; i < size; i++)
-        {
-            ptr[i] = c;
-        }
-        return ptr;
-    }
+	if (ptr == NULL || size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		unsigned int i;
+
+		for (i = 0; i < size; i++)
+		{
+			ptr[i] = c;
+		}
+		return (ptr);
+	}
 }
