d936fbc312685a11fe999093c7263c0d1ba086d7
nadazkai21 <nadazaki019@gmail.com>
2023-11-02 18:29:58 +0200

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,26 +8,28 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    void *ptr;
-    unsigned int i;
-    if (nmemb == 0 || size == 0)
-    {
-        return (NULL);
-    }
-    
-    ptr = malloc(size * nmemb);
-    if (ptr == NULL)
-    {
-        return (NULL);
-    }
-    else
-    {
-        char *byte_ptr = (char *)ptr;
-        for (i = 0; i < size * nmemb; i++)
-        {
-            byte_ptr[i] = 0;
-        }
-    }
-    
-    return (ptr);
+	void *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		char *byte_ptr = (char *)ptr;
+
+		for (i = 0; i < size * nmemb; i++)
+		{
+			byte_ptr[i] = 0;
+		}
+	}
+	return (ptr);
 }
