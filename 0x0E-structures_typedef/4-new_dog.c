1f50b0d029803243498b241a40cb7f7ffd113e53
nadazkai21 <nadazaki019@gmail.com>
2023-11-06 22:24:48 +0200

    44

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,27 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
+ * copy - copies an array of chars from stack to heap
+ * @ptr: the pointe to the array of chars to e copies from stack
+ * Return: a pointer to the new version in the heap
+*/
+char *copy(char *ptr)
+{
+	int i = 0, len = 0;
+
+	while (ptr[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	char *heap_copy = malloc(sizeof(char) * (len + 1));
+
+	for (i = 0; i < len; i++)
+	{
+		heap_copy[i] = ptr[i];
+	}
+}
+/**
  * new_dog - creates a new dog
  * @name: name of dog
  * @age: age of dog
@@ -9,8 +30,8 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *dog_name = name;
-	char *dog_owner = owner;
+	char *dog_name_heap = copy(name);
+	char *dog_owner_heap = copy(owner);
 
 	dog_t *my_dog = malloc(sizeof(dog_t));
 
@@ -20,9 +41,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	else
 	{
-		my_dog->name = dog_name;
+		my_dog->name = dog_name_heap;
 		my_dog->age = age;
-		my_dog->owner = dog_owner;
+		my_dog->owner = dog_owner_heap;
 	}
 
 	return (my_dog);
