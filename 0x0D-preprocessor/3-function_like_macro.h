e66b520e9efc335de84dddbd9461b1561c8072a2
nadazkai21 <nadazaki019@gmail.com>
2023-11-06 16:50:46 +0200

    3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,10 @@
-#ifndef ABS
+/**
+ * ABS - get the absolute of the number
+ * @x: the number to get the absolute of
+*/
+#ifndef ABS_H
+#define ABS_H
+
 #define ABS(x) ((x) >= (0) ? (x) : (x * (-1)))
 
 #endif
