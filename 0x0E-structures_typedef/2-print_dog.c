ab08ac464a00f105102082c1e96ebe52dd082273
nadazkai21 <nadazaki019@gmail.com>
2023-11-06 19:31:53 +0200

    2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - strint attributes in dof in struct dog
+ * @d: pointer to struct
+*/
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		printf(" ");
+	}
+	else
+	{
+		if (((*d).name) != NULL)
+		{
+			printf("Name: %s\n", ((*d).name));
+		}
+
+		if (((*d).age) != 0)
+		{
+			printf("Age: %f\n", ((*d).age));
+		}
+
+		if (((*d).owner) != NULL)
+		{
+			printf("Owner: %s\n", ((*d).owner));
+		}
+	}
+}
