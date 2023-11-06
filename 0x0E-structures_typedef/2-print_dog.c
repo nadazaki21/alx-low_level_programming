e11f9cd76a984872486b01a8a2169ff31f2e7201
nadazkai21 <nadazaki019@gmail.com>
2023-11-06 19:32:44 +0200

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,11 +17,6 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", ((*d).name));
 		}
 
-		if (((*d).age) != 0)
-		{
-			printf("Age: %f\n", ((*d).age));
-		}
-
 		if (((*d).owner) != NULL)
 		{
 			printf("Owner: %s\n", ((*d).owner));
