363fc59c837fac2e36f9c1a14185cfb66f4372c7
nadazkai21 <nadazaki019@gmail.com>
2023-11-06 23:02:10 +0200

    4, fixing trial, checkin gif name or owner is null

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -44,6 +44,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (my_dog == NULL)
 	return (NULL);
 
+	if (name == NULL || owner == NULL)
+	{
+		return NULL;
+	}
+	
+
 	if (name != NULL)
 	{
 		i = 0;
