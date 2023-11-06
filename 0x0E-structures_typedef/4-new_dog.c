e580da1ef68652da7de3d43ef591540e525f7a37
nadazkai21 <nadazaki019@gmail.com>
2023-11-06 23:09:29 +0200

    4, fixing trial,fee on name or owner = null, failure on copying

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -58,6 +58,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		if (my_dog-> name == NULL)
 		{
 			free(my_dog-> name);
+			free(my_dog);
 			return (NULL);
 		}
 	}
@@ -83,6 +84,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		if (my_dog-> owner == NULL)
 		{
 			free(my_dog-> owner);
+			free(my_dog);
 			return (NULL);
 		}
 		
