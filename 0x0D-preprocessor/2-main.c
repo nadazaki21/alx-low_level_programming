e9fed0058571b79ec2e32d36942b20c096a5e3a6
nadazkai21 <nadazaki019@gmail.com>
2023-11-06 16:20:33 +0200

    q33

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - ain function to print the name of the
+ * current file
+ * Return: 0 on success, other number on failure
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
