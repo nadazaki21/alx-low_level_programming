40264be4952594c85f4f02f09aaecaf407773182
nadazkai21 <nadazaki019@gmail.com>
2023-11-06 18:56:27 +0200

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef MYSTRUCT_H
+#define MYSTRUCT_H
+/**
+ * struct dog - has pointer elements to attributes of dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#endif
