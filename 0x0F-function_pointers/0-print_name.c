66c4fa54e69e50066b516e20bb3e0a7aac73abed
nadazkai21 <nadazaki019@gmail.com>
2023-11-08 19:08:28 +0200

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,9 @@
+/**
+ * print_name - function that prints a name
+ * @f: the pointer to the function that prints a string
+ * @name: the string to be prtinted and sent to the function f
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,7 @@
+#ifndef MY_HEADER
+
+#define MY_HEADER
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
