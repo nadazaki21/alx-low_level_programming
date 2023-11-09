243f6e56ef9faf18e6b7ee2662e148ad0b35c920
nadazkai21 <nadazaki019@gmail.com>
2023-11-09 19:46:46 +0200

    0

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,22 @@
+#include  <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * 
+*/
+int main(int ac , char **av)
+{
+    int num1, num2 , operator;
+
+    if (ac != 4)
+    {
+            printf("Error\n");
+            exit(98);
+    }
+    
+    num1 = atoi(av[1]);
+    num2 = atoi(av[3]);
+    
+
+   
+}
---
+++ 0x10-variadic_functions/main.h
@@ -0,0 +1,6 @@
+#ifndef MY_HEADER
+#define MY_HEADER
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
