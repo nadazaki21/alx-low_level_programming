577d4f9abd43a18101efa4a7f442797bdc925450
nadazkai21 <nadazaki019@gmail.com>
2023-11-16 14:09:12 +0200

    fixing task 2 advanced

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,27 @@
 section .data
-	hello_message db "Hello, Holberton", 0   ; Null-terminated string
+hello_message db 'Hello, Holberton', 0xA, 0
+format db '%s', 0
 
 section .text
-	global main
-	extern printf
+global main
+extern printf
 
 main:
-	sub rsp, 8   ; Align stack to 16 bytes boundary
+    ; Save the value of the rsp register before the function call
+    push rsp
 
-	; Call printf with the format string
-	mov rdi, hello_message
-	call printf
+    ; First argument (format string)
+    lea rdi, [rel format]
 
-	add rsp, 8   ; Restore stack pointer
-	ret
+    ; Second argument (address of hello_message string)
+    lea rsi, [rel hello_message]
+
+    ; Call printf
+    call printf
+
+    ; Restore the value of the rsp register
+    pop rsp
+
+    ; Return 0 from main
+    xor eax, eax
+    ret
