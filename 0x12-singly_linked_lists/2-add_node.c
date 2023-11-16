019b75f48d7f2b5409a51406c79ba237729b937a
nadazkai21 <nadazaki019@gmail.com>
2023-11-16 13:46:19 +0200

    fixing task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,7 +11,7 @@ list_t *add_node(list_t **head, const char *str)
 	/* head is an array of pointer to stucts*/
 	/* (*head) would mean the address of a specific struct*/
 	int i = 0, len = 0;
-	list_t *new_node, *tmp;
+	list_t *new_node;
 
 	/* create a new node*/
 	new_node = malloc(sizeof(list_t));
@@ -37,8 +37,8 @@ list_t *add_node(list_t **head, const char *str)
 	/*add the new node to the beginning*/
 	if ((*head) != NULL)
 	{
-		new_node->next = head;
-		head = new_node;
+		new_node->next = *head;
+		*head = new_node;
 	}
 	else
 		*(head) = new_node;
