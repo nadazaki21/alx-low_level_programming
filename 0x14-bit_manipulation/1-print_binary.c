8c7259a06529961986529957d4cdaa9144245518
nadazkai21 <nadazaki019@gmail.com>
2023-11-24 00:53:45 +0200

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,24 +4,24 @@
  * @n: the number to be converted to binary
 */
 void print_binary(unsigned long int n)
-{   
-    if (n == 0)
-    {
-        printf("0");
-        return;
-    }
-    else if (n == 1)
-    {
-        printf("1");
-        return;
-    }
+{
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+	else if (n == 1)
+	{
+		printf("1");
+		return;
+	}
 
-    n = n >> 1;
-    if (n == 0)
-    {
-        return;
-    }
+	n = n >> 1;
+	if (n == 0)
+	{
+		return;
+	}
 
-    print_binary(n);
-    printf("%ld", n & 1);
+	print_binary(n);
+	printf("%ld", n & 1);
 }
