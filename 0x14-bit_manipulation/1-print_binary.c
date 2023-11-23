0a67cfe6286fe4e66cfd922d079881329d56f954
nadazkai21 <nadazaki019@gmail.com>
2023-11-24 00:51:46 +0200

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_binary - function that prints the binary representation of a number.
+ * @n: the number to be converted to binary
+*/
+void print_binary(unsigned long int n)
+{   
+    if (n == 0)
+    {
+        printf("0");
+        return;
+    }
+    else if (n == 1)
+    {
+        printf("1");
+        return;
+    }
+
+    n = n >> 1;
+    if (n == 0)
+    {
+        return;
+    }
+
+    print_binary(n);
+    printf("%ld", n & 1);
+}
