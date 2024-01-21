d7f94882e3d049377304f35110854fa8d2dd062d
nadazkai21 <nadazaki019@gmail.com>
2024-01-21 14:14:11 +0200

    1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_dlistint - print_dlistint
+ * @h: head of the list
+ * Return: the number of nodes in the lists
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes_count = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	while (h != NULL)
+	{
+		nodes_count++;
+		h = h->next;
+	}
+
+	return (nodes_count);
+}
