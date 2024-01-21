d4be348c432901bfa32f745e2f29e9403e8dcc41
nadazkai21 <nadazaki019@gmail.com>
2024-01-21 14:10:38 +0200

    0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,15 +7,11 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes_count = 0;
-	/*dlistint_t *cursor;*/
 
 	if (h == NULL)
 	{
-		printf("-> 0 elements");
 		return (0);
 	}
-
-	/*cursor = h;*/
 
 	while (h != NULL)
 	{
@@ -24,7 +20,6 @@ size_t print_dlistint(const dlistint_t *h)
 		nodes_count++;
 		h = h->next;
 	}
-	/*printf("-> %ld elements", nodes_count);*/
 
 	return (nodes_count);
 }
