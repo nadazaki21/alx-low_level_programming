8b6ab77e529a2f302f8059ec80e65e450e22b004
nadazkai21 <nadazaki019@gmail.com>
2024-05-05 16:02:51 +0300

    1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,59 @@
+#include "search_algos.h"
+/**
+ * binary_search - function that searches for a value in a sorted array of integers using the Binary search algorithm
+ * @array: pointer to first elemt in an array
+ * @size: the size fo the array
+ * @value: the value to serach for
+*/
+
+int binary_search(int *array, size_t size, int value)
+{   
+	size_t middle;
+	size_t i = 0;
+	int ret;
+
+	if (array == NULL)
+		return (-1);
+
+	if (size % 2 == 0)
+		middle = (size/ 2) - 1;
+	else
+		middle = (size/ 2);
+
+	printf("Searching in array: ");
+	for (i = 0; i < size; i++)
+	{   
+		if (i == (size - 1))
+		{
+			printf("%d", array[i]);
+		}
+		else
+		{
+			printf("%d, ", array[i]);
+		}
+	}
+	printf("\n");
+	/*
+	printf("middle is: %ld \n", middle);
+	printf("size is: %ld\n", size);
+	printf("array[middle] = %d\n", array[middle]);
+	printf("value is: %d\n", value);
+	*/
+	if (array[middle] == value)
+		return (middle);
+	
+	if (size == 1 && array[middle] != value)
+		return (-1);
+
+	if (array[middle] < value)
+	{
+		ret = binary_search((array + middle + 1), (size - middle  - 1) , value);
+		if (ret != -1)
+			return (ret + (size - middle  - 1));
+		else
+			return (-1);
+	}
+	else 
+		ret = binary_search(array, (size - middle - 1 -1 ), value);
+	return ret;
+}
