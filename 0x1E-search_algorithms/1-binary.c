4cc2abb716a06b1299daa0154a4d85bd162d45ea
nadazkai21 <nadazaki019@gmail.com>
2024-05-05 16:14:42 +0300

    1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,13 +1,15 @@
 #include "search_algos.h"
 /**
- * binary_search - function that searches for a value in a sorted array of integers using the Binary search algorithm
+ * binary_search - function that searches for a value
+ * in a sorted array of integers using the Binary search algorithm
  * @array: pointer to first elemt in an array
  * @size: the size fo the array
  * @value: the value to serach for
+ * Return: the index of the value to srearch for
 */
 
 int binary_search(int *array, size_t size, int value)
-{   
+{
 	size_t middle;
 	size_t i = 0;
 	int ret;
@@ -16,44 +18,35 @@ int binary_search(int *array, size_t size, int value)
 		return (-1);
 
 	if (size % 2 == 0)
-		middle = (size/ 2) - 1;
+		middle = (size / 2) - 1;
 	else
-		middle = (size/ 2);
+		middle = (size / 2);
 
 	printf("Searching in array: ");
 	for (i = 0; i < size; i++)
-	{   
+	{
 		if (i == (size - 1))
-		{
 			printf("%d", array[i]);
-		}
 		else
-		{
 			printf("%d, ", array[i]);
-		}
 	}
 	printf("\n");
-	/*
-	printf("middle is: %ld \n", middle);
-	printf("size is: %ld\n", size);
-	printf("array[middle] = %d\n", array[middle]);
-	printf("value is: %d\n", value);
-	*/
+
 	if (array[middle] == value)
 		return (middle);
-	
+
 	if (size == 1 && array[middle] != value)
 		return (-1);
 
 	if (array[middle] < value)
 	{
-		ret = binary_search((array + middle + 1), (size - middle  - 1) , value);
+		ret = binary_search((array + middle + 1), (size - middle  - 1), value);
 		if (ret != -1)
 			return (ret + (size - middle  - 1));
 		else
 			return (-1);
 	}
-	else 
-		ret = binary_search(array, (size - middle - 1 -1 ), value);
-	return ret;
+	else
+		ret = binary_search(array, (size - middle - 1 - 1), value);
+	return (ret);
 }
